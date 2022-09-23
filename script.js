cfe789f833b796a0fae3c87270487586877cedb1
Dayse Campos <111383784+dayseCampos@users.noreply.github.com>
2022-09-22 22:18:33 -0300

    retirando uma linha que não é necessária

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let menu  = document.querySelector("#icone-menu");
 let barraNav = document.querySelector(".barra-menu");
-let logo = document.querySelector(".logo")
 
 menu.addEventListener("click", function () {
     barraNav.classList.toggle("active");
